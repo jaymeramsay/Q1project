2522a5bfcda2ddc32e7be97f3f9c3e672405cfd1
Jayme Ramsay <jayme.ramsay@gmail.com>
2017-09-02 13:47:37 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//Canvas creation
+let canvas = document.querySelector('canvas');
+
+canvas.width = '700'; //window.innerWidth stretches to the whole window left/right
+canvas.height = '500'; //window.innerHeight stretches to the whole window up/down
+
+let context = canvas.getContext('2d'); //canvas object uses method getContext to access to a lot of drawing info
+
+//Side Bar
+
+let clothingBar = document.getElementById('clothingBar');
+let utilityBar = document.getElementById('utilityBar');
+
+//Clothing Div??
