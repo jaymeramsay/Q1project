92bb3de54aba82aae826f48177da6001b967ebf3
Jayme Ramsay <jayme.ramsay@gmail.com>
2017-09-03 18:01:15 -0500

    modified canvas and assets

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 //Canvas creation
 let canvas = document.querySelector('canvas');
 
-canvas.width = '700'; //window.innerWidth stretches to the whole window left/right
-canvas.height = '500'; //window.innerHeight stretches to the whole window up/down
+// canvas.width = '500'; //window.innerWidth stretches to the whole window left/right
+// canvas.height = '700'; //window.innerHeight stretches to the whole window up/down
 
 let context = canvas.getContext('2d'); //canvas object uses method getContext to access to a lot of drawing info
 
+// // IMAGE OBJECT
+// let sectionImageObj = {
+//   templates: ["humanModelPic.png"],
+//   assessories: [],
+//   dresses: [],
+//   tops: [],
+//   bottoms: []
+// };
+//
+// // SETTING UP KONVA LIBRARY FOR CANVAS
+// let stage = new Konva.Stage({
+//   container: '#canvas',
+//   width: canvas.width,
+//   height: canvas.height
+// });
+//
+// let firstLayer = new Konva.Layer();
+//
+// stage.add(firstLayer);
+//
+// let secondLayer = new Konva.Layer();
+
+
+// Create events for all of template and clothing images
+
+
 //Side Bar
-
-let clothingBar = document.getElementById('clothingBar');
-let utilityBar = document.getElementById('utilityBar');
-
-//Clothing Div??
