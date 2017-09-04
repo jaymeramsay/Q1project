9701121bbd0d151715617392f1c0cd851d7fd6e6
Jayme Ramsay <jayme.ramsay@gmail.com>
2017-09-03 21:42:24 -0500

    created forEach for my images, created tags for each image and appended to DOM

--- app.js
+++ app.js
@@ -1,19 +1,58 @@
 //Canvas creation
 let canvas = document.querySelector('canvas');
-
-// canvas.width = '500'; //window.innerWidth stretches to the whole window left/right
-// canvas.height = '700'; //window.innerHeight stretches to the whole window up/down
-
 let context = canvas.getContext('2d'); //canvas object uses method getContext to access to a lot of drawing info
 
-// // IMAGE OBJECT
-// let sectionImageObj = {
-//   templates: ["humanModelPic.png"],
-//   assessories: [],
-//   dresses: [],
-//   tops: [],
-//   bottoms: []
-// };
+
+// IMAGE OBJECT
+let sectionImageObj = {
+  templates: [
+    'assets/humanModelPic.png',
+    'http://fillmurray.com/150/150',
+    'http://fillmurray.com/150/150',
+    'http://fillmurray.com/150/150',
+    'http://fillmurray.com/150/150',
+  ],
+  accessories: [
+    'http://fillmurray.com/150/150',
+    'http://fillmurray.com/150/150',
+    'http://fillmurray.com/150/150',
+    'http://fillmurray.com/150/150',
+  ],
+  dresses: [
+    'http://fillmurray.com/150/150',
+    'http://fillmurray.com/150/150',
+    'http://fillmurray.com/150/150',
+    'http://fillmurray.com/150/150',
+  ],
+  tops: [
+    'http://fillmurray.com/150/150',
+    'http://fillmurray.com/150/150',
+    'http://fillmurray.com/150/150',
+    'http://fillmurray.com/150/150',
+  ],
+  bottoms: [
+    'http://fillmurray.com/150/150',
+    'http://fillmurray.com/150/150',
+    'http://fillmurray.com/150/150',
+    'http://fillmurray.com/150/150',
+  ]
+};
+
+Object.keys(sectionImageObj).forEach(function (section) {
+  let imageSelector = document.querySelector(`#${section} .card`);
+  console.log(imageSelector);
+
+  sectionImageObj[section].forEach(function (value) {
+    //   // create img html element with src = value
+    let imageElement = document.createElement('img');
+    imageElement.src = value;
+    // attach image to section div
+    imageSelector.appendChild(imageElement);
+    console.log(imageSelector);
+
+    // put event listener on image
+  });
+});
 //
 // // SETTING UP KONVA LIBRARY FOR CANVAS
 // let stage = new Konva.Stage({
@@ -31,5 +70,7 @@ let context = canvas.getContext('2d'); //canvas object uses method getContext to
 
 // Create events for all of template and clothing images
 
+
+//Side Bar
 
 //Side Bar
